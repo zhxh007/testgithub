2a0449c4c0ba238b0970eec26a007ed12e9b6cc2
zhangxiuhui <zhangxiuhui@zhangxiuhuideMacBook-Pro.local>
2018-04-10 16:48:03 +0800

    Initial commit

---
+++ test/src/main/java/test/App.java
@@ -0,0 +1,18 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("");
+
+        List<String> l = new ArrayList<String>();
+
+    }
+
+
+}
+
---
+++ test/src/main/java/test/TestImpl.java
@@ -0,0 +1,10 @@
+package test;
+
+public class TestImpl implements Test {
+
+
+    @Override
+    public void test() {
+        System.out.println();
+    }
+}
